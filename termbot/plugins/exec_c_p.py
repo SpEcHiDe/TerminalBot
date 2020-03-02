57bc1e3faf6289721bbddff35ffe5b0acc00628d
Shrimadhav U K <shrimadhavuk@gmail.com>
2020-03-02 23:03:54 +0530

    update copyright notice

--- termbot/plugins/exec_c_p.py
+++ termbot/plugins/exec_c_p.py
@@ -113,6 +113,23 @@ async def kill_cmd_t(client, message):
         await message.reply_text("No command is running in that message.", quote=True)
 
 
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 def hash_msg(message):
     return str(message.chat.id) + "/" + str(message.message_id)
 
